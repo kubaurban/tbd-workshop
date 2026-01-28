3d4177a6f78f63ace937b570272494cbc94d3614
Mateusz Tobiasz <mati11.2001@gmail.com>
2026-01-28 23:10:07 +0100

    finished the whole phase 2

--- notebooks/benchmark_polars.py
+++ notebooks/benchmark_polars.py
@@ -3,24 +3,29 @@ import time
 import pandas as pd
 import os
 
-df_pl = pl.read_parquet("social_media_data.parquet")
-df_pl_users = pl.read_parquet("users.parquet")
+df_pl = pl.read_parquet(
+    "/mnt/d/Studia - magisterskie/tbd-workshop/notebooks/social_media_data.parquet"
+)
+df_pl_users = pl.read_parquet(
+    "/mnt/d/Studia - magisterskie/tbd-workshop/notebooks/users.parquet"
+)
 
 threads = pl.thread_pool_size()
 print(f"Polars threads: {threads}")
 
+
 def query_A_polars():
-    return df_pl.group_by('location').agg(pl.col('likes').mean()).height
+    return df_pl.group_by("location").agg(pl.col("likes").mean()).height
+
 
 def query_B_polars():
     return (
-        df_pl
-        .sort(["user_id", "timestamp"])
+        df_pl.sort(["user_id", "timestamp"])
         .with_columns(
             pl.col("likes")
-              .rolling_mean(window_size=3, min_samples=1)
-              .over("user_id")
-              .alias("avg_last_3")
+            .rolling_mean(window_size=3, min_samples=1)
+            .over("user_id")
+            .alias("avg_last_3")
         )
         .filter(pl.col("avg_last_3") > 5000)
         .height
@@ -28,7 +33,12 @@ def query_B_polars():
 
 
 def query_C_polars():
-    return df_pl.join(df_pl_users, on="user_id", how="inner").filter(pl.col("age") >= 25).height
+    return (
+        df_pl.join(df_pl_users, on="user_id", how="inner")
+        .filter(pl.col("age") >= 25)
+        .height
+    )
+
 
 queries = {
     "A polars": query_A_polars,
@@ -53,15 +63,20 @@ def benchmark(queries):
         # benchmark
         t = measure_time(query_fn)
 
-        results.append({
-            "Threads": threads,
-            "Query": query_name,
-            "Time [s]": t,
-        })
+        results.append(
+            {
+                "Threads": threads,
+                "Query": query_name,
+                "Time [s]": t,
+            }
+        )
 
     return pd.DataFrame(results)
+
 
 benchmark_results = benchmark(queries)
 print(benchmark_results)
 write_header = not os.path.exists("benchmark_polars.csv")
-benchmark_results.to_csv("benchmark_polars.csv", mode="a", header=write_header, index=False)
+benchmark_results.to_csv(
+    "benchmark_polars.csv", mode="a", header=write_header, index=False
+)
